b56f6810ece554d3cd834a7285e0ad07807a6034
David <david.mosca69@gmail.com>
2019-04-08 17:48:13 +0200

    Le binding fonctionne sur la classe Modele !!!

--- WpfApp/Model/Dto/ModeleDto.cs
+++ WpfApp/Model/Dto/ModeleDto.cs
@@ -7,14 +7,65 @@ namespace WpfApp.Model.Dto
     [Table("Modele")]
     public class ModeleDto : CommunDto
     {
-        [Required]
-        public bool IsStackable { get; set; } = false;
+        #region SiPoco
+        //[Required]
+        //public bool IsStackable { get; set; } = false;
 
-        public int CategorieId { get; set; }
+        //public int CategorieId { get; set; }
+
+        //[ForeignKey("CategorieId")]
+        //public virtual CategorieDto CategorieDto {get; set;}
+
+        //public virtual ICollection<InWorldDto> InWorldsDto { get; set; }
+        #endregion
+
+        #region SiDto
+        private bool _isStackable = false;
+        private int _categorieId;
+        private CategorieDto _categorieDto;
+
+        [Required]
+        public bool IsStackable
+        {
+            get => _isStackable;
+            set
+            {
+                if (value != _isStackable)
+                {
+                    _isStackable = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public int CategorieId
+        {
+            get => _categorieId;
+            set
+            {
+                if (value != _categorieId)
+                {
+                    _categorieId = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
 
         [ForeignKey("CategorieId")]
-        public virtual CategorieDto CategorieDto {get; set;}
+        public virtual CategorieDto CategorieDto
+        {
+            get => _categorieDto;
+            set
+            {
+                if (value != _categorieDto)
+                {
+                    _categorieDto = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
 
         public virtual ICollection<InWorldDto> InWorldsDto { get; set; }
+        #endregion
     }
 }
