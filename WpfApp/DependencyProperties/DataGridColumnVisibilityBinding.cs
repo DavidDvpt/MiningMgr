56faca8671aaa8da3ec9f16aa11f8d0e34f7ff00
David <david.mosca69@gmail.com>
2019-04-19 12:15:09 +0200

    Ajout du style button

--- WpfApp/DependencyProperties/DataGridColumnVisibilityBinding.cs
+++ WpfApp/DependencyProperties/DataGridColumnVisibilityBinding.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace WpfApp
+namespace WpfApp.DependencyProperties
 {
     public class DataGridColumnVisibilityBinding : Freezable
     {
