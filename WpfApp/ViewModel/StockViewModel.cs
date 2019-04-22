35833db27e239e95dedb8a53a877700eb7d1a635
David <david.mosca69@gmail.com>
2019-04-22 17:49:53 +0200

    Push avant teste de userControl réutilisable

--- WpfApp/ViewModel/StockViewModel.cs
+++ WpfApp/ViewModel/StockViewModel.cs
@@ -1,20 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WpfApp.Model;
 
 namespace WpfApp.ViewModel
 {
     public class StockViewModel : BaseViewModel
     {
+        #region Champs
+
+        private string _dgItemSourceParam;
+
+        #endregion
+
+        #region Constructeurs et initialisation
+
         public StockViewModel(string param)
         {
-
+            _dgItemSourceParam = param;
         }
+
         protected override void Init()
         {
-            //throw new NotImplementedException();
+            Modeles = repos.Modeles.GetByCategorieName("Material").ToList();
         }
+
+        #endregion
+
+        #region Gestion du modele à afficher
+
+        public ICollection<Modele> Modeles { get; set; }
+
+        public Modele SelectedModele
+        {
+            get { return GetValue(() => SelectedModele); }
+            set
+            {
+                if (value != SelectedModele)
+                {
+                    SetValue(() => SelectedModele, value);
+                    ItemSourceUpdated();
+                }
+            }
+        }
+
+        #endregion
+
+        #region DataGrid
+
+        public ICollection<Material> DataGridItemSource
+        {
+            get { return GetValue(() => DataGridItemSource); }
+            set
+            {
+                if (value != DataGridItemSource)
+                {
+                    SetValue(() => DataGridItemSource, value);
+                }
+            }
+        }
+
+        private void ItemSourceUpdated()
+        {
+            DataGridItemSource = repos.Materials.GetByModeleId(SelectedModele.Id);
+        }
+        
+        #endregion
     }
 }
