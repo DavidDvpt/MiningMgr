72d1f6513299e7c5b312697b0290497124da37e3
David <david.mosca69@gmail.com>
2019-04-25 18:41:24 +0200

    fonctionne pas

---
+++ WpfApp/ControlUsers/FormulaireAchatUserControl.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp.ControlUsers
+{
+    /// <summary>
+    /// Logique d'interaction pour FormulaireAchatUserControl.xaml
+    /// </summary>
+    public partial class FormulaireAchatUserControl : UserControl
+    {
+        public FormulaireAchatUserControl()
+        {
+            InitializeComponent();
+        }
+
+        protected void Validation_Error(object sender, ValidationErrorEventArgs e)
+        {
+            if (e.Action == ValidationErrorEventAction.Added) BindableBase.Errors += 1;
+            if (e.Action == ValidationErrorEventAction.Removed) BindableBase.Errors -= 1;
+        }
+    }
+}
