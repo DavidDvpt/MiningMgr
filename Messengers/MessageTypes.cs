4eaf3aee8e6c4a01a9e5734449e5032e6ac83ac8
David <david.mosca69@gmail.com>
2019-04-27 17:36:30 +0200

    Ajout de fichier au projet messages

---
+++ Messengers/MessageTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace Messengers
+{
+    /// <summary>
+    /// Use an enumeration for the messages to ensure consistency.
+    /// 
+    /// </summary>
+    public enum MessageTypes
+    {
+        MSG_CUSTOMER_SELECTED_FOR_EDIT,	// Sent when a Customer is selected for editing
+        MSG_CUSTOMER_SAVED				// Sent when a Customer is updated to the repository
+    };
+}
