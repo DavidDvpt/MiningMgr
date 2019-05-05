038f28909741b5da1b0044a7f5b7ba991a504abf
David <david.mosca69@gmail.com>
2019-05-05 11:01:00 +0200

    Modification de unique (validation)

--- ViewModels/AttributValidation/Unique.cs
+++ ViewModels/AttributValidation/Unique.cs
@@ -16,21 +16,21 @@ namespace ViewModels
             Type t = validationContext.ObjectType;
             MiningContext ctx = new MiningContext();
             // Recuperation de l'id de l'item
-            //int actualId;
-            //actualId = ((UnstackableEditViewData)validationContext.ObjectInstance).Id > 0 ? ((UnstackableEditViewData)validationContext.ObjectInstance).Id : 0;
+            int actualId;
+            actualId = ((UnstackableEditViewData)validationContext.ObjectInstance).Id > 0 ? ((UnstackableEditViewData)validationContext.ObjectInstance).Id : 0;
 
             //Verification que le Nom n'existe pas avec un id different
             // si on verifie pas l'il, la notification Unique se declenche si simmple modif de l'item
 
-            //var contains = ctx.Communs.Any(x => x.Nom == value.ToString());
-            //if (contains)
-            //{
+            var contains = ctx.Communs.Any(x => x.Nom == value.ToString());
+            if (contains)
+            {
                 return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
-            //}
-            //else
-            //{
-            //    return ValidationResult.Success;
-            //}
+            }
+            else
+            {
+                return ValidationResult.Success;
+            }
         }
     }
 }
