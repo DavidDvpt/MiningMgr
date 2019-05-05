bdcd7e62ce65aff84a9c485a7e40cce87563acad
David <david.mosca69@gmail.com>
2019-05-06 00:06:47 +0200

    modif seed class

--- WpfApp/Migrations/Configuration.cs
+++ WpfApp/Migrations/Configuration.cs
@@ -3,6 +3,7 @@ namespace WpfApp.Migrations
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.IO;
     using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WpfApp.Context.MiningContext>
@@ -14,6 +15,8 @@ namespace WpfApp.Migrations
 
         protected override void Seed(WpfApp.Context.MiningContext context)
         {
+            string script = File.ReadAllText(@"H:\Data\script.sql");
+            context.Database.ExecuteSqlCommand(script);
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
